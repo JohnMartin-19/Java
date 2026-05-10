2659497893568879df5b029e8f1b7cddc982a571
John Mburu <johnnkonge2020@gmail.com>
2026-05-10 08:43:26 +0300

    First java hello wrld

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+//    public static void main(String[] args) {
+//        System.out.println("Hello John!. This is my first Java program since Uni.");
+//    }
+
+      public static void main (String[] args) {
+        System.out.println("Niaje cutie. Na umeiva");
+      }
+}
+
