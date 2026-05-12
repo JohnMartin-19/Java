ffff36fae60440b0c72e841af094c77e220dc741
John Mburu <johnnkonge2020@gmail.com>
2026-05-12 11:06:38 +0300

    working on Datatypes in JAVA

--- Main.java
+++ Main.java
@@ -8,6 +8,20 @@ public class Main{
         System.out.println("Woow");
         String name = "John";
         System.out.println("Hello" + name);
+
+        //Data types
+        String jina = "Shanice";
+        int studentID =14020;
+        double score = 420.00;
+        char grade = 'A';
+
+        System.out.println(jina);
+        System.out.println(studentID);
+        System.out.println(score);
+        System.out.println(grade);
       }
+
+
+      
 }
 
