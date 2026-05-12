9530d1e262ffe9eb4ecb1285a9c2d97570e0c091
John Mburu <johnnkonge2020@gmail.com>
2026-05-12 13:07:23 +0300

    practicing string concat in Java

--- Main.java
+++ Main.java
@@ -19,6 +19,24 @@ public class Main{
         System.out.println(studentID);
         System.out.println(score);
         System.out.println(grade);
+
+        //strings
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(letters.length());
+        System.out.println(letters.toLowerCase());
+        System.out.println(letters.toUpperCase());
+        System.out.println(letters.indexOf("D"));
+        System.out.println(letters.indexOf("Z"));
+
+
+        //concatenation of strings
+        String firstName = "Shanice";
+        String lastName = "Wanjiru";
+        var jinaFull = firstName + " " + lastName;
+        String fullName = firstName.concat(lastName);
+        System.out.println(fullName);
+        System.out.println(jinaFull);
+
       }
 
 
