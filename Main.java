1b208584c0a3160ad0eb90562436f8915c37c291
John Mburu <johnnkonge2020@gmail.com>
2026-05-12 10:57:18 +0300

    created my first java program since Uni

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ public class Main{
 
       public static void main (String[] args) {
         System.out.println("Niaje cutie. Na umeiva");
+        System.out.println("Woow");
+        String name = "John";
+        System.out.println("Hello" + name);
       }
 }
 
