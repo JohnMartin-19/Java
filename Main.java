c60a986eb4186f5d85c2871cac18eb754da1e914
John Mburu <johnnkonge2020@gmail.com>
2026-05-13 10:25:15 +0300

    Worked on loops and arrays in Java

--- Main.java
+++ Main.java
@@ -37,9 +37,86 @@ public class Main{
         System.out.println(fullName);
         System.out.println(jinaFull);
 
+
+        //comparison of booleans
+        int myAge = 24;
+        int ageToVote = 18;
+
+        if(myAge >= ageToVote){
+            System.out.println("You are eligible to vote");
+        }else {
+            System.out.println("You are not eligible to vote");
+        }
+
+        int legalAge = 20;
+        String checker = (myAge > legalAge) ? "Legal" : "minor"; 
+        System.out.println(checker);
+
+        //print even numbers using for loop
+        var i = 0;
+        for (i=0; i< 30; i=i+2){
+          System.out.println(i);
+        }
+        //print odd numbers
+        var j =1;
+        for (j=1; j< 30; j=j+2){
+          System.out.println(j);
+        }
+
+        //for ...each loops
+        String[] cars = {"Atenzaa","Athlete","Benz","BMW", "RS6"};
+
+        for(String car: cars){
+          System.out.println(car);
+        }
+
+        int[] numbers = {1,4,90,12,45,21,87};
+
+        for (int number:numbers){
+          //var biggest = Math.max(number);
+          System.out.println(number);
+        }
+
+
+        //break.. continue with a for loop
+        for(int number:numbers){
+          if (number < 5){
+            continue;
+          }
+          if (number == 21){
+            break;
+          }
+          System.out.println(number);
+        }
+
+        //arrays
+        String names[] = {"mburu","mike","zac","ip"};
+        names[2]= "symoh";
+        System.out.println(names[2]);
+        System.out.println(names.length);
+
+        //looping thru the array with the for loop
+          for (int n=0; n < names.length; n++){
+            System.out.println(names[n]);
+          }
+        
+
+          //looping thru an array and coming up with the summation
+          int sum = 0;
+
+          for(int l=0; l<numbers.length; l++){
+            sum += numbers[l];
+            System.out.println(sum);
+          }
+
+
+
+
+
+
       }
 
-
+      
       
 }
 
