98f75eebff53e29241a3d5b84e944a46ac28e4d6
John Mburu <johnnkonge2020@gmail.com>
2026-05-15 12:39:51 +0300

    worked on a polymorphism example

--- Packages/Main.java
+++ Packages/Main.java
@@ -2,14 +2,41 @@ package Packages;
 
 import java.util.Scanner;
 
+ ///polymorphism
+ class Animal{
+    public void animalSound(){
+        System.out.println("The animal makes a sound");
+    }
 
+}
+
+class Pig extends Animal{
+    public void animalSound(){
+        System.out.println("The pig says: wee wee");
+    }
+}
+
+class Dog extends Animal{
+    public void animalSound(){
+        System.out.println("Dog barks");
+    }
+}
 class Main{
+   
     public static void main(String[] args) {
         Scanner myObject = new  Scanner(System.in);
         System.out.println("Enter username");
 
         String username = myObject.nextLine();
         System.out.println(username);
+
+        Animal myPig = new Pig();
+        Animal myDog = new Dog();
+        Animal myAnimal = new Animal();
+
+        myAnimal.animalSound();
+        myPig.animalSound();
+        myDog.animalSound();
 
     }
 }
@@ -29,3 +56,4 @@ class Car extends  Vehicle{
         System.out.println(myCar.brand + " " + myCar.modelName);
     }
 }
+
