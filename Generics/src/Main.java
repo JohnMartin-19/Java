8c6e95a0f8d008038068082535e26967e06012ca
John Mburu <johnnkonge2020@gmail.com>
2026-05-28 09:57:02 +0300

    Worked ona generics method

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -13,6 +13,11 @@ class Box<T> {
     }
 }
 public class Main{
+    public static <T> void printArray(T[] array){
+        for(T item: array){
+            System.out.println(item);
+        }
+    }
     public static void main(String[] args){
         //create a Box holding string datatype
         Box<String> stringBox = new Box<>();
@@ -25,5 +30,13 @@ public class Main{
         intBox.set(28);
         var outcome = intBox.get();
         System.out.println(outcome);
+
+        //Array od strings
+        String[] names = {"John", "Shanice", "Emma", "Ian", "Mike"};
+        Integer[] numbers = {28,12,43,89,2};
+
+        printArray(names);
+        printArray(numbers);
+
     }
 }
--- Java_Streams_Lambdas/src/Greeting.java
+++ Java_Streams_Lambdas/src/Greeting.java
@@ -1,4 +1,4 @@
-//an interface describes what a class can do w/ou specifying how
+//an interface describes what a class must do w/ou specifying how
 // (used for abstraction and multiple inheritance since a class is only restricted one inheritance)
 
 //a functional interface only holds 1 abstract method
